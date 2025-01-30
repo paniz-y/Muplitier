46dcd64d724e15a3b08cadc9135dbfe2ba682a63
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-01-30 21:47:39 +0330

    full adder added

---
+++ full_adder.vhdl
@@ -0,0 +1,13 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY fullAdder IS 
+    PORT (Cin, x, y : IN STD_LOGIC;
+          s, Cout   : OUT STD_LOGIC);
+END fullAdder;
+
+ARCHITECTURE logicFunc OF fullAdder IS 
+BEGIN 
+    s <= x XOR y XOR Cin;
+    Cout <= (x AND y) OR (Cin AND x) OR (Cin AND y);
+END logicFunc;
