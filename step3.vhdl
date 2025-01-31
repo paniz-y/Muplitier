f2396f591ab2e990a04026d39b910f7d8773bae7
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-01-31 20:23:22 +0330

    step 3 fixed

--- step3.vhdl
+++ step3.vhdl
@@ -8,11 +8,12 @@ ENTITY TwoByTwoMul IS
 END TwoByTwoMul;
 
 ARCHITECTURE TwoMul OF TwoByTwoMul IS
-BEGIN
-	--PROCESS(x,y) IS
-	--BEGIN
-P(0) <= x(0) AND y(0);
-P(1) <= (y(0) AND x(1)) XOR (y(1) AND x(0));
-P(2) <= (x(1) and x(0)) XOR x(0);
-P(3) <= (x(0) AND y(0)) AND (x(1) AND y(1));
+
+  BEGIN
+
+    P(0) <= x(0) AND y(0);
+    P(1) <= (y(0) AND x(1)) XOR (y(1) AND x(0));
+    P(2) <=  ((y(0) AND x(1)) AND (y(1) AND x(0))) XOR (x(1) AND y(1));
+    P(3) <= (x(0) AND y(0)) AND (x(1) AND y(1));
+    
 END ARCHITECTURE;
