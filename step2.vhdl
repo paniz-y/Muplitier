9246c67d6431bc52333d25775472b7a117d1d670
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-01-31 03:33:11 +0330

    step2 fixed

--- step2.vhdl
+++ step2.vhdl
@@ -25,19 +25,11 @@ ARCHITECTURE fourMul OF FourByFourMul IS
     END COMPONENT halfAdder;
 
     SIGNAL partialProduct : STD_LOGIC_VECTOR (15 downto 0);
-    SIGNAL carryOuts : STD_LOGIC_VECTOR (9 downto 0);
-    SIGNAL sums : STD_LOGIC_VECTOR (3 downto 0);
+    SIGNAL carryOuts : STD_LOGIC_VECTOR (11 downto 0);
+    SIGNAL sums : STD_LOGIC_VECTOR (11 downto 0);
 
 BEGIN 
-    --fourMulti : for i IN 0 TO 3 GENERATE
-      --  fourmulti : for j IN 0 TO 3 GENERATE
-      --      fm : ENTITY WORK.oneByOneMul PORT MAP (
-        --        x(i),
-          --      y(j),
-            --    partialProduct((i*4)+j)
-            --);
-       -- END GENERATE;    
-    --END GENERATE;
+
     MUL1: oneByOneMul port map (y(0), x(0), partialProduct(0));
     MUL2: oneByOneMul port map (y(0), x(1), partialProduct(1));
     MUL3: oneByOneMul port map (y(0), x(2), partialProduct(2));
@@ -58,57 +50,28 @@ BEGIN
     MUL15: oneByOneMul port map (y(3), x(2), partialProduct(14));
     MUL16: oneByOneMul port map (y(3), x(3), partialProduct(15));
 
-    --partialProduct(0) <= x (0) AND y (0);
-    --partialProduct(1) <= x (0) AND y (1);  
-    --partialProduct(2) <= x (0) AND y (2);  
-    --partialProduct(3) <= x (0) AND y (3);  
-    --partialProduct(4) <= x (1) AND y (0);  
-    --partialProduct(5) <= x (1) AND y (1);  
-    --partialProduct(6) <= x (1) AND y (2);  
-    --partialProduct(7) <= x (1) AND y (3);  
-    --partialProduct(8) <= x (2) AND y (0); 
-    --partialProduct(9) <= x (2) AND y (1); 
-    --partialProduct(10) <= x (2) AND y (2); 
-    --partialProduct(11) <= x (2) AND y (3); 
-    --partialProduct(12) <= x (3) AND y (0); 
-    --partialProduct(13) <= x (3) AND y (1); 
-    --partialProduct(14) <= x (3) AND y (2); 
-    --partialProduct(15) <= x (3) AND y (3); 
-
-
-    --P(0) <= partialProduct(0);
-    --P(1) <= partialProduct(1) XOR partialProduct(4);
-    --P(2) <= partialProduct(2) XOR partialProduct(5) XOR partialProduct(8);
-    --P(3) <= partialProduct(3) XOR partialProduct(6) XOR partialProduct(9);
-    --P(4) <= (partialProduct(7) XOR partialProduct(10)) XOR partialProduct(12);
-    --P(5) <= partialProduct(11) XOR partialProduct(13) XOR partialProduct(14);
-    --P(6) <= partialProduct(15) XOR partialProduct(13);
-    --P(7) <= partialProduct(15);
     P(0) <= partialProduct(0);
-    HA1 : halfAdder PORT MAP (partialProduct(1), partialProduct(4), P(1), carryOuts(0));
-   -- FA1 : fullAdder port map (partialProduct(1), partialProduct(4), '0', P(1), carryOuts(0));
-    FA1 : fullAdder PORT MAP (partialProduct(5), partialProduct(8), carryOuts(0), sums(0), carryOuts(1));
-    FA2 : fullAdder PORT MAP (partialProduct(2), sums(0), carryOuts(1), P(2), carryOuts(2));
-    FA3 : fullAdder PORT MAP (partialProduct(9), partialProduct(12), carryOuts(2), sums(1), carryOuts(3));
-    FA4 : fullAdder PORT MAP (partialProduct(6), sums(1), carryOuts(3), sums(2), carryOuts(4));
-    FA5 : fullAdder PORT MAP (partialProduct(3), sums(2), carryOuts(4), P(3), carryOuts(5));
-    FA6 : fullAdder PORT MAP (partialProduct(10), partialProduct(13), carryOuts(5), sums(3), carryOuts(6));
-    FA7 : fullAdder PORT MAP (partialProduct(7), sums(3), carryOuts(6), P(4), carryOuts(7));
-    FA8 : fullAdder PORT MAP (partialProduct(11), partialProduct(14), carryOuts(7), P(5), carryOuts(8));
-    HA2 : halfAdder PORT MAP (partialProduct(15), carryOuts(8), P(6), carryOuts(9));
-    --FA10 : fullAdder PORT MAP (partialProduct(15), '0', carryOuts(8), P(6), carryOuts(9));
-    P(7) <= carryOuts(9);
+    HA1 : halfAdder PORT MAP (partialProduct(1), partialProduct(4), sums(0), carryOuts(0));
+    HA2 : halfAdder PORT MAP (partialProduct(7), carryOuts(2), sums(3), carryOuts(3));
+    HA3 : halfAdder PORT MAP (sums(1), partialProduct(8), sums(4), carryOuts(4));
+    HA4 : halfAdder PORT MAP (partialProduct(12), sums(5), sums(8), carryOuts(8));
 
+    FA1 : fullAdder PORT MAP (partialProduct(2), partialProduct(5), carryOuts(0), sums(1), carryOuts(1));
+    FA2 : fullAdder PORT MAP (partialProduct(3), partialProduct(6), carryOuts(1), sums(2), carryOuts(2));
+    FA3 : fullAdder PORT MAP (partialProduct(9), sums(2), carryOuts(4), sums(5), carryOuts(5));
+    FA4 : fullAdder PORT MAP (partialProduct(10), sums(3), carryOuts(5), sums(6), carryOuts(6));
+    FA5 : fullAdder PORT MAP (partialProduct(11), carryOuts(3), carryOuts(6), sums(7), carryOuts(7));
+    FA6 : fullAdder PORT MAP (partialProduct(13), sums(6), carryOuts(8), sums(9), carryOuts(9));
+    FA7 : fullAdder PORT MAP (partialProduct(14), sums(7), carryOuts(9), sums(10), carryOuts(10));
+    FA8 : fullAdder PORT MAP (partialProduct(15), carryOuts(7), carryOuts(10), sums(11), carryOuts(11));
+    P(1) <= sums(0);
+    P(2) <= sums(4);
+    P(3) <= sums(8);
+    P(4) <= sums(9);
+    P(5) <= sums(10);
+    P(6) <= sums(11);
+    P(7) <= carryOuts(11);
 
-
-
-
-    
-
-  
-
-
-    
 END fourMul ;
 
 
