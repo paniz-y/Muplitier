9fccd1d72b7cfce3187956df99909c4e9da1f6cd
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-01-31 16:53:00 +0330

    carry outs fixed

--- forthStep.vhdl
+++ forthStep.vhdl
@@ -28,32 +28,39 @@ ARCHITECTURE behavioral OF FourByFourMul_2 IS
     ALIAS temp_y1 IS y(1 downto 0); 
 
     SIGNAL partialProduct : STD_LOGIC_VECTOR (15 downto 0); 
-    SIGNAL carryOuts : STD_LOGIC_VECTOR (9 downto 0);
-    SIGNAL sums : STD_LOGIC_VECTOR (9 downto 0); 
+    SIGNAL carryOuts : STD_LOGIC_VECTOR (11 downto 0);
+    SIGNAL sums : STD_LOGIC_VECTOR (11 downto 0); 
 
     BEGIN 
         MUL1: TwoByTwoMul PORT MAP (temp_x1, temp_y1, partialProduct(3 downto 0)); 
         MUL2: TwoByTwoMul PORT MAP (temp_x1, temp_y1, partialProduct(7 downto 4)); 
         MUL3: TwoByTwoMul PORT MAP (temp_x2, temp_y2, partialProduct(11 downto 8));
         MUL4: TwoByTwoMul PORT MAP (temp_x2, temp_y2, partialProduct(15 downto 12));
-        HA1: halfAdder PORT MAP (partialProduct(8), partialProduct(4), sums(0), carryOuts(0));
-        FA1: fullAdder PORT MAP (sums(0), partialProduct(2), carryOuts(0), sums(1), carryOuts(1));
-        FA2: fullAdder PORT MAP (partialProduct(9), partialProduct(5), carryOuts(1), sums(2), carryOuts(2));
-        FA3: fullAdder PORT MAP (sums(2), partialProduct(3), carryOuts(2), sums(3), carryOuts(3));
-        FA4: fullAdder PORT MAP (partialProduct(12), partialProduct(10), carryOuts(3), sums(4), carryOuts(4));
-        FA5: fullAdder PORT MAP (sums(4), partialProduct(6), carryOuts(4), sums(5), carryOuts(5));
-        FA6: fullAdder PORT MAP (partialProduct(13), partialProduct(11), carryOuts(5), sums(6), carryOuts(6));
-        FA7: fullAdder PORT MAP (sums(6), partialProduct(7), carryOuts(6), sums(7), carryOuts(7));
-        HA2: halfAdder PORT MAP (partialProduct(14), carryOuts(7), sums(8), carryOuts(8));
-        HA3: halfAdder PORT MAP (partialProduct(15), carryOuts(8), sums(9), carryOuts(9));
+
+        HA1: halfAdder PORT MAP (partialProduct(4), partialProduct(2), sums(0), carryOuts(0));
+        --FA1: fullAdder PORT MAP (sums(0), partialProduct(2), carryOuts(0), sums(1), carryOuts(1));
+        FA2: fullAdder PORT MAP (partialProduct(5), partialProduct(3), carryOuts(0), sums(1), carryOuts(1));
+        HA2: halfAdder PORT MAP (partialProduct(6), carryOuts(1), sums(2), carryOuts(2));
+        HA3: halfAdder PORT MAP (partialProduct(7), carryOuts(2), sums(3), carryOuts(3));
+
+        HA4: halfAdder PORT MAP (partialProduct(8), sums(0), sums(4), carryOuts(4));
+        FA3: fullAdder PORT MAP (partialProduct(9), sums(1), carryOuts(4), sums(5), carryOuts(5));
+        FA4: fullAdder PORT MAP (partialProduct(10), sums(2), carryOuts(5), sums(6), carryOuts(6));
+        FA5: fullAdder PORT MAP (partialProduct(11),sums(3) , carryOuts(6), sums(7), carryOuts(7));
+
+        HA5: halfAdder PORT MAP (partialProduct(12), sums(6), sums(8), carryOuts(8));
+        FA6: fullAdder PORT MAP (partialProduct(13), sums(7), carryOuts(8), sums(9), carryOuts(9));
+        FA7: fullAdder PORT MAP (partialProduct(14), carryOuts(9), carryOuts(7), sums(10), carryOuts(10));
+        HA6: halfAdder PORT MAP (partialProduct(15), carryOuts(10), sums(11), carryOuts(11));
+
 
     P(0) <= partialProduct(0); 
     P(1) <= partialProduct(1); 
-    P(2) <= sums(1); 
-    P(3) <= sums(3); 
-    P(4) <= sums(5); 
-    P(5) <= sums(7); 
-    P(6) <= sums(8); 
-    P(7) <= sums(9); 
+    P(2) <= sums(4); 
+    P(3) <= sums(5); 
+    P(4) <= sums(8); 
+    P(5) <= sums(9); 
+    P(6) <= sums(10); 
+    P(7) <= sums(11); 
 
 END behavioral ;
